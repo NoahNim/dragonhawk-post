93fc8381d79db2490e0102854d26f6aec2811798
noahnim <nmedoff123@gmail.com>
2022-07-05 21:21:11 -0700

    added firebase cli

---
+++ src/Firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBEP_gYJmCE_TaGoYjlD3k4s7UFc_v_Ho",
+
+  authDomain: "the-dragonhawk-post.firebaseapp.com",
+
+  projectId: "the-dragonhawk-post",
+
+  storageBucket: "the-dragonhawk-post.appspot.com",
+
+  messagingSenderId: "1048261346933",
+
+  appId: "1:1048261346933:web:b93db35dda186d54c49a3c",
+
+  measurementId: "G-4ZXCV2MF8Z",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+// const analytics = getAnalytics(app);
