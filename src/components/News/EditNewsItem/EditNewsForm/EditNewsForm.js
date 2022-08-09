dde6926a61a8400b24dfb6c8f83a34fc145ce974
noahnim <nmedoff123@gmail.com>
2022-08-09 16:57:07 -0700

    styled edit news and new news post

--- src/components/News/EditNewsItem/EditNewsForm/EditNewsForm.js
+++ src/components/News/EditNewsItem/EditNewsForm/EditNewsForm.js
@@ -8,7 +8,9 @@ import {
   Box,
   Center,
   FormErrorMessage,
+  Tooltip,
 } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
 import FirestoreContext from "../../../../store/firestore-context";
 import { auth } from "../../../../Firebase";
 
@@ -69,14 +71,24 @@ const EditNewsForm = (props) => {
             type="text"
             value={headlineInput}
             onChange={headlineInputHandler}
+            backgroundColor="#ECF8F8"
           />
           <FormLabel htmlFor="content">Content</FormLabel>
           <Textarea
             height="400px"
             value={contentInput}
             onChange={contentInputHandler}
+            backgroundColor="#ECF8F8"
           />
-          <Button type="submit">Edit</Button>
+          <Button background="none" type="submit" marginLeft="80%">
+            <Tooltip label="Edit">
+              <EditIcon
+                _hover={{ cursor: "pointer" }}
+                color="blue"
+                margin="5px"
+              />
+            </Tooltip>
+          </Button>
         </form>
       </Box>
     </Center>
